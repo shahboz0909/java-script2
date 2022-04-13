1ea6d8174aee49c8e2347c50569683fb600816f5
shahboz0909 <shahbozahmadov8@gmail.com>
2022-04-13 11:45:32 +0500

    add end

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@ var kiritish =parseInt(prompt("yoshingizni kiriting"), 10);
 
 if (kiritish >= yoshi) {
   var start = document.createElement("p");
-  var start = "Siz musoboqada ishtirok etishingiz mumkin";
+  start.textContent = "Siz musoboqada ishtirok etishingiz mumkin";
   console.log(start)
   document.body.append(start)
 }
 
 else {
   var end = document.createElement("p");
-  var end = `Uzr! yoshingiz yetmadi ${yoshi - kiritish} yildan so'ng qatnashishingiz mumkin`;
+  end.textContent = `Uzr! yoshingiz yetmadi ${yoshi - kiritish} yildan so'ng qatnashishingiz mumkin`;
   console.log(end);
   document.body.append(end);
 }
